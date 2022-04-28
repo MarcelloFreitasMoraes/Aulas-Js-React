a033322fdba95843f907aca601402f1b4c2ce2d1
marcelo freitas <mamoressp@gmail.com>
2022-04-28 16:46:17 -0300

    lista de dados

---
+++ estruturas.js
@@ -0,0 +1,36 @@
+const celulas =  [
+    {
+        Pokemon: 'Charmander',
+        Type: 'Fire',
+        Advantage_versus_element_grass: true,
+        Advantage_versus_element_water_ground_rock: false,
+        Numero: 004,
+       Category:'lizard',
+       evolution: {
+       evolution_second: 'Charmilion',
+       evolution_third: 'Charizard',
+    },
+
+    {
+        Pokemon: 'Squirtle',
+        Type: 'water',
+        Advantage_versus_element_grass_eletric: true,
+        Advantage_versus_element_fire_: false,
+        Numero: 007,
+       Category:'Tiny Turtle',
+       evolution: {
+       evolution_second: 'Wartortle',
+       evolution_third: 'Blastoise',
+    },
+
+    {
+        Pokemon: 'Bulbasaur',
+        Type: 'Grass',
+        Advantage_versus_element__ground_rock: true,
+        Advantage_versus_element_fire_psychic_flyng_ice: false,
+        Numero: 001,
+       Category:'Seed',
+       evolution: {
+       evolution_second: 'Ivysaur',
+       evolution_third: 'Venusaur',
+    }
