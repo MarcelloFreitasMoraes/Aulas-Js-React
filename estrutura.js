48e22f3a99c5004638863e0ed9335d70faa311c9
marcelo freitas <mamoressp@gmail.com>
2022-04-28 15:42:59 -0300

    exercicio estrutura de dados

---
+++ estrutura.js
@@ -0,0 +1,26 @@
+const boolean = true
+const string = "Felipe"
+const number = 10
+const float =10.1
+const sem_resultado = undefined
+const nula = null
+const array = [] //listagem
+const objeto ={}//envolver 1 unica coisa
+
+const celulas =  [
+    {
+        lider: 'Fulano',
+        numero_celula: 10,
+        mais_de_10: true
+    },
+    {
+        lider: "Fulano 2",
+        numero_celula: 11,
+        mais_de_10: false,
+        membros: ["joao", "maria", "zezinho"]
+    }
+]
+
+//Exercício
+
+//1 - Lista, 3 objetos, 7 dados, 3 strings, 2 dados boolean, 1 number, 1 objeto(2 dados, strings)
