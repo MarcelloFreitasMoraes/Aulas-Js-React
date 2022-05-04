8df6ff6d1d93382b6987f689e4a4a85b47eb5e7a
Marcelo de Freitas Moraes <mamoressp@gmail.com>
2022-05-04 10:30:07 -0300

    Delete funcao directory

--- funcao/src/utils/index.jsx
+++
@@ -1,7 +0,0 @@
-export const Consol = (valor) => {
-  console.log(`${valor} é muito louco`)
-  }
-
-  export const Click = (menssagem, subMensagem) => {
-    alert(`${menssagem} Olha ${subMensagem} TOP DEMAIS!`)
-  }
